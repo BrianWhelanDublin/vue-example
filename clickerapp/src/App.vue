58f08c395288d5c6c9b3a8798d1642a25a49cdbc
Brian Whelan <brian@friday.ie>
2021-05-04 21:09:01 +0100

    initial commit

---
+++ clickerapp/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h2>{{ state.count }}, doubled = {{ state.double }}</h2>
+  <button @click="UpdateCount(true)">+</button>
+  <button @click="UpdateCount(false)">-</button>
+</template>
+
+<script>
+import { reactive, computed } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+      double: computed(() => state.count * 2),
+    });
+    function UpdateCount(increase) {
+      increase ? state.count++ : state.count--;
+    }
+    return {
+      state,
+      UpdateCount,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button {
+  height: 20px;
+  width: 40px;
+}
+</style>
